1b1b457f90bd0685b519ec7317464e0583fb1bbb
Ahmed0Mohamed <eng.ahmed.muhamed000@gmail.com>
2021-06-08 18:30:13 +0200

    Done

---
+++ 2dTask/Class2d.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2dTask
+{
+    class Class2d
+    {
+        double[,] result = new double[3, 1];
+
+        public void Transformation(double x, double y, double t1, double t2)
+        {
+
+            double[,] array1 = new double[,] { { 1, 0, t1 }, { 0, 1, t2 }, { 0, 0, 1 } };
+            double[,] array2 = new double[,] { { x }, { y }, { 1 } };
+
+
+            multiply(array1, array2);
+
+        }
+
+        public void Scaling(double x, double y, double s1, double s2)
+        {
+
+            double[,] array1 = new double[,] { { s1, 0, 0 }, { 0, s2, 0 }, { 0, 0, 1 } };
+            double[,] array2 = new double[,] { { x }, { y }, { 1 } };
+
+
+            multiply(array1, array2);
+
+        }
+
+        public void RotationAroundAxis(double x, double y, double theta)
+        {
+            double[,] array1;
+            double[,] array2 = new double[,] { { x }, { y }, { 1 } };
+
+            array1 = new double[,] { {Math.Cos((Math.PI / 180) * theta), -Math.Sin((Math.PI / 180) * theta), 0 },{ Math.Sin((Math.PI / 180) * theta), Math.Cos((Math.PI / 180) * theta), 0 }, { 0, 0, 1 } };
+            multiply(array1, array2);
+        }
+
+        public void multiply(double[,] array1, double[,] array2)
+        {
+            double temp = 0;
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    temp = 0;
+                    for (int k = 0; k < array1.GetLength(1); k++)
+                    {
+                        temp += array1[i, k] * array2[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+        }
+        public void Reflection(double x, double y, double Xplane, double yplane)
+        {
+            double[,] array1;
+            double[,] array2 = new double[,] { { x }, { y }, { 1 } };
+            if (Xplane == 1)
+            {
+                array1 = new double[,] { { -1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1}};
+            }
+            else if (yplane == 1)
+            {
+                array1 = new double[,] { { 1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 1 }};
+
+            }
+            else
+            {
+                array1 = new double[,] { { -1, 0, 0 }, { 0, -1, 0}, { 0, 0, 1 } };
+            }
+            multiply(array1, array2);
+        }
+        public void Shearing(double x, double y, double sh1, double sh2)
+        {
+            double[,] array2 = new double[,] { { x }, { y },{ 1} };
+            double[,] array1 = new double[,] { { 1, sh1, 0 }, { sh2, 1, 0 }, { 0, 0, 1 } };
+            
+            multiply(array1, array2);
+        }
+        public void Print()
+        {
+            Console.WriteLine("New Point\n");
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.WriteLine(result[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2dTask/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _2dTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Class2d Operation2d = new Class2d();
+
+                Console.WriteLine("Please enter a choice\n 1-Transformation\n 2-Scaling\n 3-Reflection\n 4-Rotation Arround Axis\n 5-shearing\n 6-exit\n");
+                string val;
+                val = Console.ReadLine();
+                double x, y;
+                Console.WriteLine("Please enter x and y\n");
+                x = Convert.ToDouble(Console.ReadLine());
+                y = Convert.ToDouble(Console.ReadLine());
+                if (val == "1")
+                {
+                    Console.WriteLine("Please enter t1 and t2\n");
+                    double t1, t2;
+
+                    t1 = Convert.ToDouble(Console.ReadLine());
+                    t2 = Convert.ToDouble(Console.ReadLine());
+                    Operation2d.Transformation(x, y, t1, t2);
+
+
+                }
+                else if (val == "2")
+                {
+                    Console.WriteLine("Please enter t1 and t2\n");
+                    double s1, s2;
+                    s1 = Convert.ToDouble(Console.ReadLine());
+                    s2 = Convert.ToDouble(Console.ReadLine());
+                    Operation2d.Scaling(x, y,s1, s2);
+
+                }
+                else if (val == "3")
+                {
+                    Console.WriteLine("Please choose the reflection 1- X-axis , 2- y-axis or 3- Both 'Write Your choice number'\n");
+
+                    val = Console.ReadLine();
+                    if (val == "1")
+                    {
+                        Operation2d.Reflection(x, y, 1, 0);
+                    }
+                    else if (val == "2")
+                    {
+                        Operation2d.Reflection(x, y, 0,1);
+                    }
+                    else
+                    {
+                        Operation2d.Reflection(x, y,1, 1);
+                    }
+
+
+                }
+                else if (val == "4")
+                {
+                    Console.WriteLine("Please choose the rotation angle\n");
+                    double angle;
+                    angle = Convert.ToDouble(Console.ReadLine());
+
+                        Operation2d.RotationAroundAxis(x, y, angle);
+
+                }
+                else if (val == "5")
+                {
+                    Console.WriteLine("Please choose the shearing in which axis\n 1-x axis\n 2-y axis\n ");
+                    val = Console.ReadLine();
+                    double sh1;
+                    if (val == "1")
+                    {
+                        Console.WriteLine("Please enter shear x\n");
+                        sh1 = Convert.ToDouble(Console.ReadLine());
+                        Operation2d.Shearing(x, y, sh1,0);
+                    }
+                    else if (val == "2")
+                    {
+                        Console.WriteLine("Please enter shear y \n");
+                        sh1 = Convert.ToDouble(Console.ReadLine());
+                        Operation2d.Shearing(x, y, 0, sh1);
+                    }
+       
+                }
+                else if (val == "6")
+                {
+                   break;
+                }
+                else
+                {
+                    Console.WriteLine("invalid choice\n");
+                    continue;
+                }
+                Operation2d.Print();
+            }
+        }
+    }
+}
